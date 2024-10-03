e4f12712aa97ea21f2a776af63ed66c18e0a3abe
omaherne <oma32@pitt.edu>
2024-10-03 07:36:04 -0400

    Updated movement
    can now move mouse to rotate, and bot moves in direction of camera

--- turtlebot-simulation/turtle_bot.gd
+++ turtlebot-simulation/turtle_bot.gd
@@ -1,11 +1,33 @@
 extends RigidBody3D
 
+var mouse_sensitivity := 0.001
+var twist_input := 0.0
+var pitch_input := 0.0
+
+@onready var twist_pivot := $TwistPivot
+@onready var pitch_pivot := $TwistPivot/PitchPivot
+
 func _ready() -> void:
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta: float) -> void:
 	var input := Vector3.ZERO
-	input.x = Input.get_axis("rotate_left", "rotate_right") # moves the bot left instead of turning, will get to it later
+	input.x = Input.get_axis("move_left", "move_right") # moves the bot left instead of turning, will get to it later
 	input.z = Input.get_axis("move_forward", "move_backward")
 	
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(twist_pivot.basis * input * 1200.0 * delta) # basis aligns with camera so the player moves relative to camera perspective
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-30), deg_to_rad(30)) # keeps the camera from going below the bot (30 degrees)
+	twist_input = 0.0
+	pitch_input = 0.0
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = - event.relative.x * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity
