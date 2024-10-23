27026f30506b3cd9004ebb500995de28830a70ee
omaherne <oma32@pitt.edu>
2024-10-23 17:41:50 -0400

    bot update

--- turtlebot-simulation/turtle_bot/turtle_bot.gd
+++ turtlebot-simulation/turtle_bot/turtle_bot.gd
@@ -1,29 +1,33 @@
 extends VehicleBody3D
 
-@export var engine_force_value := 5.0
+@export var engine_force_value := 500.0
+@export var turn_speed := 0.8
 @onready var left_wheel = $"Left Wheel"
 @onready var right_wheel = $"Right Wheel"
 @onready var front_wheel = $"Front Wheel"
 
 var left = 0
 var right = 0
-var right_rotate = 0
-var left_rotate = 0
 
 func _physics_process(delta: float):
 	if Input.is_action_pressed("move_forward"):
 		left = Input.get_action_strength("move_forward")
 		right = Input.get_action_strength("move_forward")
-	if Input.is_action_pressed("move_backward"):
-		left = Input.get_action_strength("move_backward")
-		right = Input.get_action_strength("move_backward")
+	
+	elif Input.is_action_pressed("move_backward"):
+		left = -Input.get_action_strength("move_backward")
+		right = -Input.get_action_strength("move_backward")
+	
 	if Input.is_action_pressed("rotate_left"):
-		left = -Input.get_action_strength("rotate_left")
-		right = 1
-	if Input.is_action_pressed("rotate_right"):
-		right = -Input.get_action_strength("rotate_right")
-		left = 1
-	left_wheel.engine_force = left * engine_force_value
-	right_wheel.engine_force = right * engine_force_value
+		left = -Input.get_action_strength("rotate_left") * turn_speed
+		right = 1 * turn_speed
+	
+	elif Input.is_action_pressed("rotate_right"):
+		right = -Input.get_action_strength("rotate_right") * turn_speed
+		left = 1 * turn_speed
+	
+	left_wheel.engine_force = left * engine_force_value * delta
+	right_wheel.engine_force = right * engine_force_value * delta
+	
 	left = left_wheel.brake
 	right = right_wheel.brake
