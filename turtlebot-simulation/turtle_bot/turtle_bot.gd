47a5b40175df39ea7313f03a99278822c4f919f9
omaherne <oma32@pitt.edu>
2024-11-06 15:02:41 -0500

    Bot damping updates

--- turtlebot-simulation/turtle_bot/turtle_bot.gd
+++ turtlebot-simulation/turtle_bot/turtle_bot.gd
@@ -1,36 +1,34 @@
 extends VehicleBody3D
 
-@export var engine_force_value := 600.0
-@export var turn_speed := 0.8
-@export var limit_angular_velocity = 0.9
+@export var engine_force_value := 12.0
+@export var turn_speed := 0.75
 @export var brake_strength = 0.08
 
 @onready var left_wheel = $"Left Wheel"
 @onready var right_wheel = $"Right Wheel"
-@onready var front_wheel = $"Front Wheel"
-
-var left = 0
-var right = 0
 
 func _physics_process(delta: float):
-	if Input.is_action_pressed("move_forward"):
-		left = Input.get_action_strength("move_forward")
-		right = Input.get_action_strength("move_forward")
+	left_wheel.set_brake(0.06)
+	right_wheel.set_brake(0.06)
 	
-	elif Input.is_action_pressed("move_backward"):
-		left = -Input.get_action_strength("move_backward")
-		right = -Input.get_action_strength("move_backward")
+	if (Input.is_anything_pressed() == false):
+		left_wheel.engine_force = 0
+		right_wheel.engine_force = 0
+		left_wheel.brake
+		right_wheel.brake
+	
+	if Input.is_action_pressed("move_forward"):
+		left_wheel.engine_force = Input.get_action_strength("move_forward") * engine_force_value
+		right_wheel.engine_force = Input.get_action_strength("move_forward") * engine_force_value
+	
+	if Input.is_action_pressed("move_backward"):
+		left_wheel.engine_force = -Input.get_action_strength("move_backward") * engine_force_value
+		right_wheel.engine_force = -Input.get_action_strength("move_backward") * engine_force_value
+	
+	if Input.is_action_pressed("rotate_right"):
+		left_wheel.engine_force = Input.get_action_strength("rotate_right") * turn_speed * engine_force_value
+		right_wheel.engine_force = -Input.get_action_strength("rotate_right") * turn_speed * engine_force_value
 	
 	if Input.is_action_pressed("rotate_left"):
-		left = -Input.get_action_strength("rotate_left") * turn_speed
-		right = turn_speed
-	
-	elif Input.is_action_pressed("rotate_right"):
-		right = -Input.get_action_strength("rotate_right") * turn_speed
-		left = turn_speed
-	
-	left_wheel.engine_force = left * engine_force_value * delta
-	right_wheel.engine_force = right * engine_force_value * delta
-	
-	left = 0
-	right = 0
+		left_wheel.engine_force = -Input.get_action_strength("rotate_left") * turn_speed * engine_force_value
+		right_wheel.engine_force = Input.get_action_strength("rotate_left") * turn_speed * engine_force_value
