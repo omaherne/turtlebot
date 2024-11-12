631525726ad45e6b918354668b1e47ba920275b9
omaherne <oma32@pitt.edu>
2024-11-11 23:59:08 -0500

    Added toon shader, graffiti and bug fixes
    Lots of work done, and seems to play pretty well

--- turtlebot-simulation/objects/win_pad.gd
+++ turtlebot-simulation/objects/win_pad.gd
@@ -1,4 +1,4 @@
-extends Area3D
+extends Node3D
 
 const FILE_START = "res://levels/level_"
 
@@ -6,5 +6,8 @@ func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("TurtleBot"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
 		var next_level_number = current_scene_file.to_int() + 1 # gets the number at the end of the level name
+		if next_level_number == 6:
+			print("TurtleBot Maze Complete!")
+			next_level_number = 0
 		var next_level_path = FILE_START + str(next_level_number) + ".tscn"
-		get_tree().change_scene_to_file(next_level_path)
+		get_tree().call_deferred("change_scene_to_file", next_level_path)
